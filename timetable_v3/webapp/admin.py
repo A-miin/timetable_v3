514240564d436343c7a67daf17fad1ec14bc8272
A-miin <zulaykaisaeva@gmail.com>
2021-02-25 16:27:01 +0600

    feat(admin,-models,-fixtures,-settings): fixed a bit models, added fixtures from old database

--- timetable_v3/webapp/admin.py
+++ timetable_v3/webapp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Department, Faculty, Building, RoomType, Teacher, ClassRoom, CourseType,Editor, Course, CourseVsRoom, GradeYear, TimeTableGenerator, TimeDay, TimeHour, TimeTable
 
+admin.site.register(Department)
+admin.site.register(Faculty)
+admin.site.register(RoomType)
+admin.site.register(Teacher)
+admin.site.register(ClassRoom)
+admin.site.register(Building)
+admin.site.register(CourseType)
+admin.site.register(Course)
+admin.site.register(CourseVsRoom)
+admin.site.register(GradeYear)
+admin.site.register(TimeTableGenerator)
+admin.site.register(Editor)
+admin.site.register(TimeDay)
+admin.site.register(TimeHour)
+admin.site.register(TimeTable)
 # Register your models here.
--- timetable_v3/webapp/models.py
+++ timetable_v3/webapp/models.py
@@ -1,5 +1,6 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
 from django.db import models
+
 
 
 class Building(models.Model):
@@ -8,13 +9,19 @@ class Building(models.Model):
     short_name = models.CharField(max_length=255, db_column='shortName')
     distance_number = models.IntegerField(db_column='distanceNumber')
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         db_table = 'building'
 
 
 class Faculty(models.Model):
     name = models.CharField(max_length=255)
-    building = models.ForeignKey(to=Building, related_name='faculties', default=None, null=True, blank=True)
+    building = models.ForeignKey(to=Building, related_name='faculties', on_delete=models.SET_NULL, default=None, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         db_table = 'faculty'
@@ -26,16 +33,22 @@ class UserFaculty(models.Model):
 
 
 class Department(models.Model):
-    faculty = models.ForeignKey(to=Faculty, related_name='departments', default=None, null=True, blank=True)
-    name = models.CharField(max_length=255)
+    faculty = models.ForeignKey(to=Faculty, related_name='departments', on_delete=models.SET_NULL, default=None, null=True, blank=True)
+    name = models.CharField(max_length=255, null=True)
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         db_table = 'department'
 
 
 class RoomType(models.Model):
-    type = models.CharField(max_length=255)
-    type_code = models.IntegerField(db_column='typeCode')
+    type = models.CharField(max_length=255, null=True)
+    type_code = models.IntegerField(db_column='typeCode', null=True)
+
+    def __str__(self):
+        return self.type
 
     class Meta:
         db_table = 'room_type'
@@ -43,11 +56,14 @@ class RoomType(models.Model):
 
 class Teacher(models.Model):
     user = models.ForeignKey(to=User, related_name='teachers', on_delete=models.SET_NULL, null=True, blank=True)
-    code = models.IntegerField(unique=True, db_column='sicil')
-    name = models.CharField(max_length=255)
-    unvan = models.CharField(max_length=255, db_column='unvan')
-    extra_data = models.CharField(max_length=255, db_column='extraData')
-    employee_type = models.IntegerField(db_column='employeeType')
+    code = models.IntegerField(db_column='sicil', null=True, blank=True)
+    name = models.CharField(max_length=255, null=True, blank=True)
+    unvan = models.CharField(max_length=255, db_column='unvan', null=True, blank=True)
+    extra_data = models.CharField(max_length=255, db_column='extraData', null=True, blank=True)
+    employee_type = models.IntegerField(db_column='employeeType', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         db_table = 'teacher'
@@ -57,18 +73,23 @@ class ClassRoom(models.Model):
     building = models.ForeignKey(to=Building, on_delete=models.SET_NULL, related_name='classrooms', null=True, blank=True)
     department = models.ForeignKey(to=Department, on_delete=models.SET_NULL, related_name='classrooms', null=True, blank=True)
     user = models.ForeignKey(to=User, on_delete=models.SET_NULL, related_name='classrooms', null=True, blank=True)
-    name= models.CharField(max_length=255)
-    capacity = models.IntegerField()
+    name= models.CharField(max_length=255, null=True, blank=True)
+    capacity = models.IntegerField(null=True, blank=True)
     room_type = models.ForeignKey(to=RoomType, on_delete=models.SET_NULL, related_name='classrooms', null=True, blank=True, db_column='roomType_id')
 
+    def __str__(self):
+        return f'{self.building.short_name}-{self.name}'
 
     class Meta:
         db_table = 'classroom'
 
 
 class CourseType(models.Model):
-    type = models.CharField(max_length=255)
-    type_code = models.IntegerField(db_column='typeCode')
+    type = models.CharField(max_length=255, null=True)
+    type_code = models.IntegerField(db_column='typeCode', null=True)
+
+    def __str__(self):
+        return self.type
 
     class Meta:
         db_table = 'course_type'
@@ -78,22 +99,24 @@ class Course(models.Model):
     teacher = models.ForeignKey(to=Teacher, related_name='courses', on_delete=models.SET_NULL, null=True, blank=True)
     department = models.ForeignKey(to=Department, related_name='courses', on_delete=models.SET_NULL, null=True, blank=True)
     user = models.ForeignKey(to=User, related_name='courses', on_delete=models.SET_NULL, null=True, blank=True)
-    theory_hours = models.IntegerField(db_column='teorikDersSaati')
-    practice_hours = models.IntegerField(db_column='uygulamaDersSaati')
-    max_students = models.IntegerField(db_column='maxOgrenciSayisi')
-    code = models.CharField(max_length=255, db_column='dersKodu')
-    name = models.CharField(max_length=255, db_column='dersAdi')
+    theory_hours = models.IntegerField(db_column='teorikDersSaati', null=True, blank=True)
+    practice_hours = models.IntegerField(db_column='uygulamaDersSaati', null=True, blank=True)
+    max_students = models.IntegerField(db_column='maxOgrenciSayisi', null=True, blank=True)
+    code = models.CharField(max_length=255, db_column='dersKodu', null=True, blank=True)
+    name = models.CharField(max_length=255, db_column='dersAdi', null=True, blank=True)
     type = models.ForeignKey(to=CourseType, related_name='courses', on_delete=models.SET_NULL, null=True, blank=True, db_column='courseType_id')
     theory_room_type = models.ForeignKey(to=RoomType, related_name='theory_courses', on_delete=models.SET_NULL, null=True, blank=True, db_column='teorikRoomType_id')
     practice_room_type = models.ForeignKey(to=RoomType, related_name='practice_courses',on_delete=models.SET_NULL, null=True, blank=True, db_column='uygulamaRoomType_id')
-    credits = models.IntegerField(db_column='kredi')
+    credits = models.IntegerField(db_column='kredi', null=True, blank=True)
     # TODO EDIT
-    etkin = models.IntegerField(db_column='etkin')
-    sube = models.IntegerField(db_column='sube')
-    unpositioned_theory_hours = models.IntegerField(db_column='unpositionedTeorikHours')
-    unpositioned_practice_hours = models.IntegerField(db_column='unpositionedUygulamaHours')
-    semester = models.CharField(max_length=255)
+    etkin = models.IntegerField(db_column='etkin', null=True, blank=True)
+    sube = models.IntegerField(db_column='sube', null=True, blank=True)
+    unpositioned_theory_hours = models.IntegerField(db_column='unpositionedTeorikHours', null=True, blank=True)
+    unpositioned_practice_hours = models.IntegerField(db_column='unpositionedUygulamaHours', null=True, blank=True)
+    semester = models.CharField(max_length=255, null=True, blank=True)
 
+    def __str__(self):
+        return self.name
     class Meta:
         db_table = 'course'
 
@@ -102,16 +125,23 @@ class CourseVsRoom(models.Model):
     course = models.ForeignKey(to=Course, related_name='rooms', on_delete=models.SET_NULL, null=True, blank=True)
     classroom = models.ForeignKey(to=ClassRoom, related_name='courses', on_delete=models.SET_NULL, null=True, blank=True)
     user = models.ForeignKey(to=User, related_name='course_vs_rooms', on_delete=models.SET_NULL, null=True, blank=True)
-    lesson_type = models.IntegerField(db_column='lessonType')
+    lesson_type = models.IntegerField(db_column='lessonType', null=True, blank=True)
 
+    def __str__(self):
+        if self.classroom:
+            return f'{self.course.name} -> {self.classroom.name}'
+        else:
+            return self.course.name
     class Meta:
         db_table = 'course_vs_room'
 
 
 class GradeYear(models.Model):
     department = models.ForeignKey(to=Department, related_name='grade_years', on_delete=models.SET_NULL, null=True, blank=True)
-    grade = models.IntegerField()
+    grade = models.IntegerField(null=True, blank=True)
 
+    def __str__(self):
+        return f'{self.department.name} -> {self.grade}'
     class Meta:
         db_table = 'grade_year'
 
@@ -131,8 +161,8 @@ class Editor(models.Model):
     course = models.ForeignKey(to=Course, on_delete=models.SET_NULL, null=True, blank=True)
     classroom = models.ForeignKey(to=ClassRoom, on_delete=models.SET_NULL, null=True, blank=True)
     teacher = models.ForeignKey(to=Teacher, on_delete=models.SET_NULL, null=True, blank=True)
-    old_time = models.IntegerField(db_column='oldtime')
-    new_time = models.IntegerField(db_column='new_time')
+    old_time = models.IntegerField(db_column='oldtime', null=True)
+    new_time = models.IntegerField(db_column='new_time', null=True)
     department = models.ForeignKey(to=Department, on_delete=models.SET_NULL, null=True, blank=True)
     class Meta:
         db_table = 'editor'
@@ -143,6 +173,9 @@ class TimeDay(models.Model):
     order_position = models.IntegerField(db_column='orderPosition', null=True, blank=True)
     label = models.CharField(max_length=45, null=True, blank=True)
 
+    def __str__(self):
+        return self.label
+
     class Meta:
         db_table = 'timeDay'
 
@@ -150,6 +183,9 @@ class TimeDay(models.Model):
 class TimeHour(models.Model):
     order_position = models.IntegerField(db_column='orderPosition', null=True, blank=True)
     label = models.CharField(max_length=45, null=True, blank=True)
+
+    def __str__(self):
+        return self.label
 
     class Meta:
         db_table = 'timeHour'
@@ -160,8 +196,8 @@ class TimeTable(models.Model):
     classroom = models.ForeignKey(to=ClassRoom, on_delete=models.SET_NULL, null=True, blank=True)
     time_day = models.ForeignKey(to=TimeDay, on_delete=models.SET_NULL, null=True, blank=True)
     time_hour = models.ForeignKey(to=TimeHour, on_delete=models.SET_NULL, null=True, blank=True)
-    year = models.IntegerField(db_column='yil')
-    semester = models.CharField(max_length=45)
+    year = models.IntegerField(db_column='yil', null=True)
+    semester = models.CharField(max_length=45, null=True)
 
     class Meta:
         db_table = 'timeTable'
